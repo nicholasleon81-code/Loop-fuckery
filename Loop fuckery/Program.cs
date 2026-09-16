2f5ba3ab2de4d2e0163c1f97757dfe7b08e1d672
nicholasleon81@gmail.com <nicholasleon81@gmail.com>
2026-09-16 09:17:39 +0200

    Add project files.

---
+++ Loop fuckery/Program.cs
@@ -0,0 +1,20 @@
+﻿for (int i = 10; i > 0; i--)
+{
+    Console.Write($", {i}");
+}
+
+bool programOnline = true;
+while (programOnline)
+{
+    Console.WriteLine("Vad vill du göra?");
+    Console.WriteLine("Exit, ifall du vill avsluta.");
+    string smart = Console.ReadLine();
+    if (smart == "Exit")
+    {
+        programOnline = false;
+    }
+    else 
+    {
+        Console.WriteLine("HAHA, Nerd!");
+    }
+}
